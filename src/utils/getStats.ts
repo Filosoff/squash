ccab9e740d76282c7ec1a4823eefb371717c9b71
Flatformer <flatformer@gmail.com>
2024-08-20 01:47:47 +0200

    try-catch

--- src/utils/getStats.ts
+++ src/utils/getStats.ts
@@ -4,12 +4,6 @@ import { GameHistory } from "./types";
 const getWinnerScore = (loserScore: number) => Math.max(11, loserScore + 2);
 
 const getStats = () => {
-  let games: GameHistory;
-  try {
-    games = JSON.parse(localStorage.getItem("games") || "{}");
-  } catch {
-    games = {};
-  }
   const stats = {
     sessionsTotal: 0,
     gamesTotal: 0,
@@ -28,42 +22,53 @@ const getStats = () => {
     sessionsLengthLog: [] as number[],
   }
 
-  if (!Object.keys(games).length) {
+  try {
+    let games: GameHistory;
+    try {
+      games = JSON.parse(localStorage.getItem("games") || "{}");
+    } catch {
+      games = {};
+    }
+
+    if (!Object.keys(games).length) {
+      return stats;
+    }
+
+    stats.sessionsTotal = Object.keys(games).length;
+    for (const [date, log] of Object.entries(games)) {
+      stats.sessionsLengthLog.push(log.length);
+      log.forEach(g => {
+        stats.gamesTotal++;
+        const loserScore = g[1];
+        const winnerScore = getWinnerScore(loserScore);
+        if (g[0] === Players.alfa) {
+          stats.alfaScore += winnerScore;
+          stats.betaScore += loserScore;
+          stats.alfaGames++;
+          stats.alfaScoreLog.push(winnerScore);
+          stats.betaScoreLog.push(loserScore);
+        } else {
+          stats.alfaScore += loserScore;
+          stats.betaScore += winnerScore;
+          stats.betaGames++;
+          stats.alfaScoreLog.push(loserScore);
+          stats.betaScoreLog.push(winnerScore);
+        }
+      });
+    }
+
+    stats.scoreTotal = stats.alfaScore + stats.betaScore;
+    stats.alfaRateGames = (stats.alfaGames / stats.gamesTotal) * 100;
+    stats.betaRateGames = (stats.betaGames / stats.gamesTotal) * 100;
+    stats.alfaRateScore = (stats.alfaScore / stats.scoreTotal) * 100;
+    stats.betaRateScore = (stats.betaScore / stats.scoreTotal) * 100;
+
+    stats.gamesPerSession = stats.sessionsLengthLog.reduce((a, b) => a + b, 0) / stats.sessionsLengthLog.length;
+
+    return stats;
+  } catch {
     return stats;
   }
-
-  stats.sessionsTotal = Object.keys(games).length;
-  for (const [date, log] of Object.entries(games)) {
-    stats.sessionsLengthLog.push(log.length);
-    log.forEach(g => {
-      stats.gamesTotal++;
-      const loserScore = g[1];
-      const winnerScore = getWinnerScore(loserScore);
-      if (g[0] === Players.alfa) {
-        stats.alfaScore += winnerScore;
-        stats.betaScore += loserScore;
-        stats.alfaGames++;
-        stats.alfaScoreLog.push(winnerScore);
-        stats.betaScoreLog.push(loserScore);
-      } else {
-        stats.alfaScore += loserScore;
-        stats.betaScore += winnerScore;
-        stats.betaGames++;
-        stats.alfaScoreLog.push(loserScore);
-        stats.betaScoreLog.push(winnerScore);
-      }
-    });
-  }
-
-  stats.scoreTotal = stats.alfaScore + stats.betaScore;
-  stats.alfaRateGames = (stats.alfaGames / stats.gamesTotal) * 100;
-  stats.betaRateGames = (stats.betaGames / stats.gamesTotal) * 100;
-  stats.alfaRateScore = (stats.alfaScore / stats.scoreTotal) * 100;
-  stats.betaRateScore = (stats.betaScore / stats.scoreTotal) * 100;
-
-  stats.gamesPerSession = stats.sessionsLengthLog.reduce((a, b) => a + b, 0) / stats.sessionsLengthLog.length;
-
-  return stats;
 }
 
 
